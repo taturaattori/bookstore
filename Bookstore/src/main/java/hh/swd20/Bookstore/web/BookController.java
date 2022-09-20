f5a9b0ec7daccd4b6074dc0449b102505a59768f
Tatu Paukkeri <tatu.paukkeri@gmail.com>
2022-09-20 20:18:30 +0300

    list ja add

--- Bookstore/src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -3,9 +3,11 @@ package hh.swd20.Bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.swd20.Bookstore.domain.Book;
 import hh.swd20.Bookstore.domain.BookRepository;
 
 @Controller
@@ -23,5 +25,23 @@ private BookRepository repository;
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
+	}
+	
+	@RequestMapping(value = "/addbook")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
 	}
 }
