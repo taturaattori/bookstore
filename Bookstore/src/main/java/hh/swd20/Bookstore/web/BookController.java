c1ba1eaca339527d4d29a16d452ac7060fd1fe9e
Tatu Paukkeri <tatu.paukkeri@gmail.com>
2022-10-02 14:35:22 +0300

    restlist

--- Bookstore/src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -1,11 +1,15 @@
 package hh.swd20.Bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hh.swd20.Bookstore.domain.Book;
 import hh.swd20.Bookstore.domain.BookRepository;
@@ -55,5 +59,13 @@ private CategoryRepository crepository;
 		return "editbook";
 	}
 	
-	// save toiminto
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+	
+	@RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+	return repository.findById(bookId);
+	}
 }
